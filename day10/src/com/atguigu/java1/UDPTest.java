368363d9f70b632f6bb71904915d832f0918657d
DESKTOP-CIB862I\Tang YouLong <348190212@qq.com>
2021-09-23 14:07:52 +0800

    2021.9.23 网络编程 URL网络编程

--- day10/src/com/atguigu/java1/UDPTest.java
+++ day10/src/com/atguigu/java1/UDPTest.java
@@ -1,9 +1,11 @@
 package com.atguigu.java1;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
 import org.junit.Test;
 
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author Tang YouLong
@@ -18,8 +20,24 @@ public class UDPTest {
     
     //发送端
     @Test
-    public void sender() throws SocketException {
+    public void sender() throws IOException {
         DatagramSocket socket = new DatagramSocket();
+        String str = "UDP方式发送的导弹";
+        byte[] data = str.getBytes();
+        InetAddress inet = InetAddress.getLocalHost();
+        DatagramPacket packet = new DatagramPacket(data,0,data.length,inet,9090);
+        socket.send(packet);
+        socket.close();
+    }
 
+    //接收端
+    @Test
+    public void receiver() throws IOException {
+        DatagramSocket socket = new DatagramSocket(9090);
+        byte[] buffer = new byte[100];
+        DatagramPacket packet = new DatagramPacket(buffer,0,buffer.length);
+        socket.receive(packet);
+
+        System.out.println(new String(packet.getData(),0,packet.getLength()));
     }
 }
